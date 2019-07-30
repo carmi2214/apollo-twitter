d16c4e4a94bcb3525acc0729952bb5fba134829d
DoctorVoid <furmanmail@gmail.com>
2019-07-30 15:33:37 +0300

    added tweets page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { TweetsContainerComponent } from './components/tweets-container/tweets-container.component';
+import {NgxLinkifyjsModule} from 'ngx-linkifyjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TweetComponent,
+    TweetsContainerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxLinkifyjsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
